0aa41db80c833ac41ad0a2609bc623b9c1695308
Eva G <topuzzino@hotmail.de>
2018-03-31 19:54:05 +0200

    Еще корректуры

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 var formButton = document.querySelector(".popup-button");
 var popup = document.querySelector(".searching-form");
+var form = document.querySelector("form");
 				
 var checkIn = popup.querySelector("[name=checkin]");
 var checkOut = popup.querySelector("[name=checkout]");
 var adults = popup.querySelector("[name=adults]");
 
 
-	popup.classList.add("popup-hide");
+
+var isStorageSupport = true;
+var storage = "";
+try {
+	storage = localStorage.getItem("checkIn");
+	localStorage.getItem("checkOut");
+	localStorage.getItem("adults");
+} catch (err) {
+	isStorageSupport = false;
+}
+
+
+
+
+popup.classList.add("popup-hide");
 			
 formButton.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -14,14 +29,16 @@ formButton.addEventListener("click", function (evt) {
 	checkIn.focus();
 });
 
-popup.addEventListener("submit", function (evt) {
+form.addEventListener("submit", function (evt) {
 	evt.preventDefault();
 	if (!checkIn.value || !checkOut.value || !adults.value) {
 		console.log("Нужно ввести даты заезда и количество взрослых");
-		popup.classList.add("popup-error");
+		form.classList.add("popup-error");
 	} else {
-		localStorage.setItem("checkin", checkIn.value);
-		localStorage.setItem("checkout", checkOut.value);
-		localStorage.setItem("adults", adults.value);
+		if (isStorageSupport){
+			localStorage.setItem("checkin", checkIn.value);
+			localStorage.setItem("checkout", checkOut.value);
+			localStorage.setItem("adults", adults.value);
+		}
 	}
 });
